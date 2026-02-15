0291b323f9ee2f8e96384ada566f1d56ca40af9b
PUBKABOZKAX <g_churakov@list.ru>
2026-02-15 18:50:30 +0300

    make illagers constantly spawn in illager strctures

--- src/main/java/me/udnek/rpgu/item/ingredients/illagerite/IllageriteKey.java
+++ src/main/java/me/udnek/rpgu/item/ingredients/illagerite/IllageriteKey.java
@@ -1,5 +1,6 @@
 package me.udnek.rpgu.item.ingredients.illagerite;
 
+import io.papermc.paper.registry.keys.StructureKeys;
 import me.udnek.coreu.custom.component.instance.TranslatableThing;
 import me.udnek.coreu.custom.item.ConstructableCustomItem;
 import me.udnek.coreu.nms.Nms;
@@ -7,19 +8,26 @@ import me.udnek.coreu.nms.loot.condition.LootConditionWrapper;
 import me.udnek.coreu.nms.loot.entry.NmsCustomEntry;
 import me.udnek.coreu.nms.loot.pool.PoolWrapper;
 import me.udnek.coreu.nms.loot.util.ItemStackCreator;
-import me.udnek.rpgu.vanilla.StructureManaging;
 import net.kyori.adventure.key.Key;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.loot.LootTables;
 import org.jetbrains.annotations.Nullable;
 import org.jspecify.annotations.NullMarked;
 
+import java.util.Set;
 import java.util.function.Consumer;
+
+import static org.bukkit.loot.LootTables.*;
 
 @NullMarked
 public class IllageriteKey extends ConstructableCustomItem {
 
-
+    public static LootTables[] ILLAGER_LOOT_TABLES =  new LootTables[]{ PILLAGER, EVOKER, ILLUSIONER, VINDICATOR, WITCH };
+    public static Set<Key> ILLAGER_STRUCTURE_KEYS = Set.of(
+            StructureKeys.MANSION, StructureKeys.PILLAGER_OUTPOST,  Key.key("nova_structures:mangrove_witch_hut"), Key.key("nova_structures:witch_villa"),
+            Key.key("nova_structures:village_swamp"), Key.key("nova_structures:illager_camp"), Key.key("nova_structures:illager_hideout"),
+            Key.key("nova_structures:illager_manor"), Key.key("nova_structures:badlands_miner_outpost")
+    );
 
     @Override
     public String getRawId() {return "illagerite_key";
@@ -36,12 +44,12 @@ public class IllageriteKey extends ConstructableCustomItem {
     public void globalInitialization() {
         super.globalInitialization();
 
-        for (LootTables illagerLootTable: StructureManaging.ILLAGER_LOOT_TABLES) {
+        for (LootTables illagerLootTable : ILLAGER_LOOT_TABLES) {
             Nms.get().getLootTableWrapper(illagerLootTable.getLootTable()).addPool(new PoolWrapper.Builder(
                     new NmsCustomEntry.Builder(new ItemStackCreator.Custom(this))
-                        .addCondition(LootConditionWrapper.randomChange(0.05f))
-                        .addCondition(LootConditionWrapper.structure(StructureManaging.ILLAGER_STRUCTURE_KEYS))
-                        .buildAndWrap())
+                            .addCondition(LootConditionWrapper.randomChange(0.05f))
+                            .addCondition(LootConditionWrapper.structure(ILLAGER_STRUCTURE_KEYS))
+                            .buildAndWrap())
                     .build());
         }
     }
--- src/main/java/me/udnek/rpgu/vanilla/StructureManaging.java
+++ src/main/java/me/udnek/rpgu/vanilla/StructureManaging.java
@@ -8,49 +8,63 @@ import me.udnek.coreu.nms.structure.StructureSpawnOverrideWrapper;
 import me.udnek.coreu.nms.structure.StructureWrapper;
 import net.kyori.adventure.key.Key;
 import org.bukkit.entity.EntityType;
-import org.bukkit.loot.LootTables;
+import org.jspecify.annotations.NullMarked;
 
-import java.util.Set;
+import java.util.List;
 
-import static org.bukkit.loot.LootTables.*;
-import static org.bukkit.loot.LootTables.VINDICATOR;
-import static org.bukkit.loot.LootTables.WITCH;
-
+@NullMarked
 public class StructureManaging {
 
-    public static LootTables[] ILLAGER_LOOT_TABLES =  new LootTables[]{ PILLAGER, EVOKER, ILLUSIONER, VINDICATOR, WITCH };
-    public static Set<Key> ILLAGER_STRUCTURE_KEYS = Set.of(
-            StructureKeys.MANSION, StructureKeys.PILLAGER_OUTPOST,  Key.key("nova_structures:mangrove_witch_hut"), Key.key("nova_structures:witch_villa"),
-            Key.key("nova_structures:village_swamp"), Key.key("nova_structures:illager_camp"), Key.key("nova_structures:illager_hideout"),
-            Key.key("nova_structures:illager_manor"), Key.key("nova_structures:badlands_miner_outpost")
-    );
-
     public static void run(){
-        for (Key key: ILLAGER_STRUCTURE_KEYS) {
-            StructureWrapper structureWrapper = Nms.get().getStructureWrapper(key);
-            if (structureWrapper == null) continue;
-            System.out.println("///////////////////////////////////////////////////////////////////////////////");
-            System.out.println(key);
-            structureWrapper.getSpawnOverrides().forEach((a, b) -> {
-                        System.out.println(a + ": " + b.getSpawns());
-                    });
-            structureWrapper.editSpawnOverrides(
-                    (map -> {
-                        StructureSpawnOverrideWrapper wrapper = StructureSpawnOverrideWrapper.of(BoundingBoxTypeWrapper.PIECE);
-                        wrapper.addSpawn(new StructureSpawnOverrideWrapper.SpawnEntry(2, EntityType.WITCH, 1, 1));
-                        wrapper.addSpawn(new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.PILLAGER, 2, 4));
-                        map.put(MobCategoryWrapper.MONSTER, wrapper);
-                        System.out.println("------------");
-                        map.forEach((a, b) -> {
-                            System.out.println(a + ": " + b.getSpawns());
-                        });
-                        return map;
-                    }));
+        addPoolToStructureSpawnOverrides(StructureKeys.MANSION, List.of(
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.ILLUSIONER, 1, 1),
+                new StructureSpawnOverrideWrapper.SpawnEntry(2, EntityType.WITCH, 1, 2),
+                new StructureSpawnOverrideWrapper.SpawnEntry(3, EntityType.PILLAGER, 2, 3),
+                new StructureSpawnOverrideWrapper.SpawnEntry(3, EntityType.VINDICATOR, 1, 3)
+        ));
+        addPoolToStructureSpawnOverrides(Key.key("nova_structures:illager_camp"), List.of(
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.PILLAGER, 1, 1)
+        ));
+        addPoolToStructureSpawnOverrides(Key.key("nova_structures:illager_hideout"), List.of(
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.ILLUSIONER, 1, 1),
+                new StructureSpawnOverrideWrapper.SpawnEntry(2, EntityType.WITCH, 1, 1),
+                new StructureSpawnOverrideWrapper.SpawnEntry(3, EntityType.PILLAGER, 2, 3),
+                new StructureSpawnOverrideWrapper.SpawnEntry(3, EntityType.VINDICATOR, 1, 3)
+        ));
+        addPoolToStructureSpawnOverrides(Key.key("nova_structures:village_swamp"), List.of(
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.WITCH, 1, 3)
+        ));
+        addPoolToStructureSpawnOverrides(Key.key("nova_structures:witch_villa"), List.of(
+                new StructureSpawnOverrideWrapper.SpawnEntry(3, EntityType.WITCH, 1, 3),
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.SLIME, 1, 1)
+        ));
+        addPoolToStructureSpawnOverrides(Key.key("nova_structures:illager_manor"), List.of(
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.PILLAGER, 2, 3),
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.VINDICATOR, 1, 3)
+        ));
+        addPoolToStructureSpawnOverrides(Key.key("nova_structures:badlands_miner_outpost"), List.of(
+                new StructureSpawnOverrideWrapper.SpawnEntry(1, EntityType.ILLUSIONER, 1, 1),
+                new StructureSpawnOverrideWrapper.SpawnEntry(2, EntityType.WITCH, 1, 1),
+                new StructureSpawnOverrideWrapper.SpawnEntry(3, EntityType.PILLAGER, 2, 3),
+                new StructureSpawnOverrideWrapper.SpawnEntry(3, EntityType.VINDICATOR, 1, 3)
+        ));
+    }
 
-            System.out.println("------------");
-            structureWrapper.getSpawnOverrides().forEach((a, b) -> {
-                System.out.println(a + ": " + b.getSpawns());
-            });
-        }
+    public static void addPoolToStructureSpawnOverrides(Key structureKey, List<StructureSpawnOverrideWrapper.SpawnEntry> entries) {
+        addPoolToStructureSpawnOverrides(structureKey, entries, MobCategoryWrapper.MONSTER);
+    }
+
+    public static void addPoolToStructureSpawnOverrides(Key structureKey, List<StructureSpawnOverrideWrapper.SpawnEntry> entries, MobCategoryWrapper category) {
+        StructureWrapper structureWrapper = Nms.get().getStructureWrapper(structureKey);
+        if (structureWrapper == null) throw new RuntimeException("Structure not found!");
+        structureWrapper.editSpawnOverrides(
+                (map -> {
+                    StructureSpawnOverrideWrapper wrapper = StructureSpawnOverrideWrapper.of(BoundingBoxTypeWrapper.PIECE);
+                    for(StructureSpawnOverrideWrapper.SpawnEntry entry: entries){
+                        wrapper.addSpawn(entry);
+                    }
+                    map.put(category, wrapper);
+                    return map;
+                }));
     }
 }
